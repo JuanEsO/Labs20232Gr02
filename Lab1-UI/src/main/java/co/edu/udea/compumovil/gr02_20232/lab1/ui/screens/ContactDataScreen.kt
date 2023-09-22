34864e009e993708cf717dda2f9c4bb215f1ca0d
juan-urrego <juan-urrego@live.com>
2023-09-21 22:32:47 -0500

    terminando pantalla de contacto

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20232/lab1/ui/screens/ContactDataScreen.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20232/lab1/ui/screens/ContactDataScreen.kt
@@ -1,5 +1,6 @@
 package co.edu.udea.compumovil.gr02_20232.lab1.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,16 +14,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,6 +45,10 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -45,133 +56,182 @@ import co.edu.udea.compumovil.gr02_20232.lab1.R
 
 @Composable
 fun ContactDataScreen(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 22.dp)
+        .height(1000.dp)
+        .verticalScroll(rememberScrollState()),
     onNextButtonClicked: () -> Unit = {}
 ) {
+
+    var phone by remember { mutableStateOf("") }
+    var address by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var country by remember { mutableStateOf("") }
+    var city by remember { mutableStateOf("") }
+    var keyboardImmeActionCustom by remember { mutableStateOf( ImeAction.Next )}
+    keyboardImmeActionCustom = if (validateContactForm(phone = phone.toLongOrNull() ?: 0L, email = email, country = country)) {
+        ImeAction.Done
+    } else {
+        ImeAction.Next
+    }
+    var isEmailValid by remember { mutableStateOf(true) }
+    var isFormFill = validateContactForm(phone = phone.toLongOrNull() ?: 0L, email = email, country = country) && isEmailValid
+    fun onNext() {
+
+        if (isFormFill) {
+            var genderValue : String? = null
+
+            logPersonalData(phone = phone.toLongOrNull() ?: 0L,
+                email = email,
+                country = country,
+                city = city,
+                address = address)
+            onNextButtonClicked()
+        }
+    }
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
-        ) {
-            var phone by remember { mutableStateOf("") }
-            var address by remember { mutableStateOf("") }
-            var email by remember { mutableStateOf("") }
-            var country by remember { mutableStateOf("") }
-            var city by remember { mutableStateOf("") }
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.height(70.dp)
-            ) {
-                Icon(
+        Column() {
+            OutlinedTextField(
+                value = phone,
+                onValueChange = { phone = it },
+                leadingIcon = { Icon(
                     imageVector = Icons.Filled.Call,
-                    contentDescription = stringResource(R.string.back_button),
-                    modifier = Modifier.width(35.dp)
-                )
+                    contentDescription = "iconCall",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                ) },
+                label = { Text(text = "Phone") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 20.dp)
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { newText ->
+                    email = newText
+                    isEmailValid = isEmailValidEmail(newText)
 
-                Spacer(modifier = Modifier.width(10.dp))
-
-                OutlinedTextField(
-                    value = phone,
-
-                    onValueChange = { phone = it },
-                    label = { Text("Phone") },
-                    maxLines = 1,
-                    textStyle = TextStyle(
-                        color = Color.Blue,
-                        fontWeight = FontWeight.Bold,
-                        background = Color.Blue
-                        ),
-                    modifier = Modifier.width(200.dp)
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.height(70.dp)
-            ) {
-                Icon(
+                },
+                leadingIcon = { Icon(
                     imageVector = Icons.Filled.Email,
-                    contentDescription = stringResource(R.string.back_button),
-                    modifier = Modifier.width(35.dp)
-                )
-
-                Spacer(modifier = Modifier.width(10.dp))
-
-                OutlinedTextField(
-                    value = email,
-                    onValueChange = { email = it },
-                    label = { Text("Email") },
-                    maxLines = 1,
-                    textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
-                    modifier = Modifier.width(200.dp)
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.height(70.dp)
-            ) {
-                Icon(
+                    contentDescription = "email",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                ) },
+                isError = !isEmailValid,
+                label = { Text(text = "Email") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 20.dp)
+            )
+            OutlinedTextField(
+                value = country,
+                onValueChange = { newText ->
+                    country = newText
+                },
+                leadingIcon = { Icon(
                     imageVector = Icons.Filled.Home,
-                    contentDescription = stringResource(R.string.back_button),
-                    modifier = Modifier.width(35.dp)
-                )
+                    contentDescription = "country",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                ) },
+                label = { Text(text = "Pais") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 20.dp)
+            )
 
-                Spacer(modifier = Modifier.width(10.dp))
 
-                OutlinedTextField(
-                    value = country,
-                    onValueChange = { country = it },
-                    label = { Text("Country") },
-                    maxLines = 1,
-                    textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
-                    modifier = Modifier.width(200.dp)
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.height(70.dp)
-            ) {
-                Icon(
+
+            OutlinedTextField(
+                value = city,
+                onValueChange = { newText ->
+                    city = newText
+                },
+                leadingIcon = { Icon(
                     imageVector = Icons.Filled.Place,
-                    contentDescription = stringResource(R.string.back_button),
-                    modifier = Modifier.width(35.dp)
-                )
-
-                Spacer(modifier = Modifier.width(10.dp))
-
-                OutlinedTextField(
-                    value = city,
-                    onValueChange = { city = it },
-                    label = { Text("City") },
-                    maxLines = 1,
-                    textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
-                    modifier = Modifier.width(200.dp)
-                )
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.height(70.dp)
-            ) {
-                Icon(
+                    contentDescription = "ciudad",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                ) },
+                label = { Text(text = "Ciudad") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 20.dp)
+            )
+            OutlinedTextField(
+                value = address,
+                onValueChange = { newText ->
+                    address = newText
+                },
+                leadingIcon = { Icon(
                     imageVector = Icons.Filled.AccountBox,
-                    contentDescription = stringResource(R.string.back_button),
-                    modifier = Modifier.width(35.dp)
-                )
+                    contentDescription = "address",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                ) },
+                label = { Text(text = "Direccion") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 20.dp)
+            )
 
-                Spacer(modifier = Modifier.width(10.dp))
 
-                OutlinedTextField(
-                    value = address,
-                    onValueChange = { address = it },
-                    label = { Text("Address") },
-                    maxLines = 1,
-                    textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
-                    modifier = Modifier.width(200.dp)
-                )
-            }
         }
         Row(modifier = Modifier.weight(1f, false)) {
             Column(
@@ -183,7 +243,8 @@ fun ContactDataScreen(
             ) {
                 Spacer(modifier = Modifier.height(20.dp))
                 Button(
-                    onClick = onNextButtonClicked
+                    enabled = isFormFill,
+                    onClick = {onNext()}
                 ) {
                     Text("Finalizar")
                 }
@@ -192,6 +253,27 @@ fun ContactDataScreen(
             }
         }
     }
+}
+
+fun validateContactForm(phone: Number, email: String, country: String): Boolean {
+    return phone != null && email.isNotEmpty() && country.isNotEmpty()
+}
+
+fun isEmailValidEmail(email: String): Boolean {
+    // Regular expression for a simple email validation
+    val emailRegex = Regex("^[A-Za-z0-9+_.-]+@(.+)\$")
+
+    return email.matches(emailRegex)
+}
+
+fun logPersonalData(phone: Number, email: String, country: String, city : String? = null, address: String? = null) {
+    val tag = "[RESULT]"
+    Log.i(tag, "Datos personales: ")
+    Log.i(tag, "Telefono: $phone")
+    Log.i(tag, "Email: $email")
+    Log.i(tag, "pais: $country")
+    Log.i(tag, "Ciudad: $city")
+    Log.i(tag, "Direccion: $address")
 }
 
 @Preview
