a1d66f864888426581a474c1ad58c4ac3309fb54
Juanes <juanortiz@Juans-MacBook-Air.local>
2023-09-21 20:56:54 -0500

    PersonalDataScreen - validations and logs

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20232/lab1/ui/screens/PersonalDataScreen.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20232/lab1/ui/screens/PersonalDataScreen.kt
@@ -1,25 +1,26 @@
 package co.edu.udea.compumovil.gr02_20232.lab1.ui.screens
 
 import android.app.DatePickerDialog
+import android.util.Log
 import android.widget.DatePicker
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -33,7 +34,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -50,127 +50,155 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import co.edu.udea.compumovil.gr02_20232.lab1.R
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
 import java.util.Calendar
 import java.util.Date
 
 @Composable
 fun PersonalDataScreen(
+    onNextButtonClicked: () -> Unit = {},
     modifier: Modifier = Modifier
         .fillMaxSize()
         .padding(horizontal = 22.dp)
         .height(1000.dp)
         .verticalScroll(rememberScrollState())
 ) {
+    val options = listOf("Primaria", "Secundaria", "Universidad", "Otro")
+    val items = listOf<String>("ninguno", "Hombre", "Mujer")
+
     var name by remember { mutableStateOf(TextFieldValue("")) }
     var lastname by remember { mutableStateOf(TextFieldValue("")) }
+    var selectedGender by remember { mutableStateOf(0) }
+    val mBirthDate = remember { mutableStateOf("") }
+    var selectedEducation by remember { mutableStateOf(options[0]) }
+    
+    
+
     var keyboardImmeActionCustom by remember { mutableStateOf( ImeAction.Next )}
-//    if (name.text.length > 0) {
-//        keyboardImmeActionCustom = ImeAction.Next
-//    } else {
-//        keyboardImmeActionCustom = ImeAction.Done
-//    }
+
+    keyboardImmeActionCustom = if (validateForm(name = name.text, lastName = lastname.text, birthDate = mBirthDate.value, education = selectedEducation )) {
+        ImeAction.Done
+    } else {
+        ImeAction.Next
+    }
+
+    fun onNext() {
+        var isFormFill = validateForm(name = name.text, lastName = lastname.text, birthDate = mBirthDate.value, education = selectedEducation )
+        if (isFormFill) {
+            var genderValue : String? = null
+            if (selectedGender != 0) {
+                genderValue = items[selectedGender]
+            }
+            logData(name = name.text, lastName = lastname.text, gender = genderValue, birthDate = mBirthDate.value, education = selectedEducation)
+            onNextButtonClicked()
+        }
+    }
     Column(
         modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-
-        OutlinedTextField(
-            value = name,
-            onValueChange = { newText ->
-                name = newText
-            },
-            leadingIcon = { Icon(
-                imageVector = Icons.Default.Person,
-                contentDescription = "personIcon",
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.size(25.dp)
-            ) },
-            label = { Text(text = "Nombres") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = keyboardImmeActionCustom,
-                capitalization = KeyboardCapitalization.Words,
-                autoCorrect = false,
-            ),
-            shape = shapes.medium,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-        OutlinedTextField(
-            value = lastname,
-            onValueChange = { newText ->
-                lastname = newText
-            },
-            leadingIcon = { Icon(
+        Column() {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { newText ->
+                    name = newText
+                },
+                leadingIcon = { Icon(
                     imageVector = Icons.Default.Person,
                     contentDescription = "personIcon",
                     tint = MaterialTheme.colorScheme.primary,
                     modifier = Modifier.size(25.dp)
                 ) },
-            label = { Text(text = "Apellidos") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = keyboardImmeActionCustom,
-                capitalization = KeyboardCapitalization.Words,
-                autoCorrect = false,
-            ),
-            shape = shapes.medium,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-        Row( modifier = Modifier
+                label = { Text(text = "Nombres") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp, top = 20.dp)
+            )
+            OutlinedTextField(
+                value = lastname,
+                onValueChange = { newText ->
+                    lastname = newText
+                },
+                leadingIcon = { Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "personIcon",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                ) },
+                label = { Text(text = "Apellidos") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = keyboardImmeActionCustom,
+                    capitalization = KeyboardCapitalization.Words,
+                    autoCorrect = false,
+                ),
+                keyboardActions = KeyboardActions (
+                    onDone = { onNext() }
+                ),
+                shape = shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp)
+            )
+            Row( modifier = Modifier
                 .height(70.dp)
                 .fillMaxWidth()
                 .padding(start = 12.dp),
-            verticalAlignment = Alignment.CenterVertically ) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_group_24),
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.size(25.dp)
-            )
-            Spacer(modifier = Modifier.size(10.dp))
-            Text(text = "Sexo: ", modifier = Modifier.padding(end = 20.dp))
-            var selectedIndex by remember { mutableStateOf(0) }
-            val items = listOf<String>("ninguno", "Hombre", "Mujer")
-
-            items.forEachIndexed { index, item ->
-                if (index != 0) {
-                    CircleSelect(
-                        label = item,
-                        checked = items[selectedIndex] == item,
-                        onCheckedChange = { selectedIndex = index }
-                    )
+                verticalAlignment = Alignment.CenterVertically ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_group_24),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(25.dp)
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(text = "Sexo: ", modifier = Modifier.padding(end = 20.dp))
+                items.forEachIndexed { index, item ->
+                    if (index != 0) {
+                        CircleSelect(
+                            label = item,
+                            checked = items[selectedGender] == item,
+                            onCheckedChange = { selectedGender = index }
+                        )
+                    }
                 }
             }
+            DatePickerInput(mDate = mBirthDate)
+
+
+            
+            Row(
+                modifier = Modifier
+                    .height(70.dp)
+                    .fillMaxWidth()
+                    .padding(start = 12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                DropdownMenu(
+                    options = options,
+                    selectedOption = selectedEducation,
+                    onChangeOption = { newOption -> selectedEducation = newOption },
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+            }
         }
-        DatePickerInput()
-//        SpinnerInput()
-
-        val options = listOf("Primaria", "Secundaria", "Universidad", "Otro")
-
-        var selectedOptionText by remember { mutableStateOf(options[0]) }
-
-        fun onChangeOption(newOption: String) {
-            selectedOptionText = newOption
-        }
-
-        Row(
-            modifier = Modifier
-                .height(70.dp)
-                .fillMaxWidth()
-                .padding(start = 12.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            DropdownMenu(
-                options = options,
-                selectedOption = selectedOptionText,
-                onChangeOption = { newOption -> selectedOptionText = newOption },
-                modifier = Modifier.padding(bottom = 10.dp)
-            )
+        Row( horizontalArrangement = Arrangement.End, modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 50.dp)) {
+            Button(onClick = { onNext() }, modifier = Modifier.defaultMinSize(100.dp)) {
+                Text(text = "Next")
+            }
         }
     }
 }
@@ -214,7 +242,7 @@ fun CircleSelect(
 }
 
 @Composable
-fun DatePickerInput() {
+fun DatePickerInput(mDate: MutableState<String>) {
     val mContext = LocalContext.current
     val mCalendar = Calendar.getInstance()
     val mYear = mCalendar.get(Calendar.YEAR)
@@ -222,8 +250,6 @@ fun DatePickerInput() {
     val mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
 
     mCalendar.time = Date()
-
-    val mDate = remember { mutableStateOf("") }
 
     val mDatePickerDialog = DatePickerDialog(
         mContext,
@@ -276,7 +302,7 @@ fun DropdownMenu(options: List<String>, selectedOption: String, onChangeOption: 
             readOnly = true,
             value = selectedOption,
             onValueChange = { },
-            label = { Text("Categories", Modifier.background(Color.Transparent)) },
+            label = { Text("Educacion", Modifier.background(Color.Transparent)) },
             leadingIcon = {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_menu_book_24),
@@ -314,3 +340,19 @@ fun DropdownMenu(options: List<String>, selectedOption: String, onChangeOption: 
         }
     }
 }
+
+
+fun validateForm(name: String, lastName: String, birthDate: String, education: String): Boolean {
+    return name.isNotEmpty() && lastName.isNotEmpty() && birthDate.isNotEmpty() && education.isNotEmpty()
+}
+
+fun logData(name: String, lastName: String, gender: String? = null, birthDate: String, education: String) {
+    val tag = "[RESULT]"
+    Log.i(tag, "Información personal: ")
+    Log.i(tag, "Nombre: $name")
+    Log.i(tag, "Apellido: $lastName")
+    if (gender != null) Log.i(tag, "Genero: $gender")
+    Log.i(tag, "Fecha de Nacimiento: $birthDate")
+    Log.i(tag, "Educacion: $education")
+}
+
